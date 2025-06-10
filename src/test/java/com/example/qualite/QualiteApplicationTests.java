48a8b7d0bedf7c5c9d1daf3248244c7775213d41
EwenDanielo <100780740+EwenDanielo@users.noreply.github.com>
2025-06-10 14:47:14 +0200

    first commit

---
+++ src/test/java/com/example/qualite/QualiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qualite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
