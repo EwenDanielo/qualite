e682ae201f34533a42cd9e623f9a1806090d9ecb
Hugo Penché <hugo.penche@gmail.com>
2025-06-11 18:17:58 +0200

    add jenkins

--- biblioflex-api/src/main/java/com/example/qualite/QualiteApplication.java
+++ biblioflex-api/src/main/java/com/example/qualite/QualiteApplication.java
@@ -8,13 +8,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class QualiteApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
-		System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+		// Charge les variables .env seulement si elles ne sont pas déjà définies (ex: en local)
+		if (System.getenv("POSTGRES_USER") == null) {
+			Dotenv dotenv = Dotenv.load();
+			System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+			System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+			System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+			System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+		}
 
 		SpringApplication.run(QualiteApplication.class, args);
 	}
 }
+
 
