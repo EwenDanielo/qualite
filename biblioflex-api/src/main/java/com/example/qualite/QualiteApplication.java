473f0b856564412b0d36928163d62445027afa30
hug0pe <72266330+hug0pe@users.noreply.github.com>
2025-06-11 17:41:35 +0200

    Update QualiteApplication.java

--- biblioflex-api/src/main/java/com/example/qualite/QualiteApplication.java
+++ biblioflex-api/src/main/java/com/example/qualite/QualiteApplication.java
@@ -7,14 +7,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class QualiteApplication {
 
-	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
-		System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+    public static void main(String[] args) {
+        if (System.getenv("POSTGRES_USER") == null) {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+            System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+            System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+            System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+        }
 
-		SpringApplication.run(QualiteApplication.class, args);
-	}
+        SpringApplication.run(QualiteApplication.class, args);
+    }
 }
+
 
