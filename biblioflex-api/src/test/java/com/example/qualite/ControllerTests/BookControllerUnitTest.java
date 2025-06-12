5642ecd5bce484ea0b0a056352c9266fb29dbede
EwenDanielo <100780740+EwenDanielo@users.noreply.github.com>
2025-06-12 14:37:43 +0200

    udpate java tests

--- biblioflex-api/src/test/java/com/example/qualite/ControllerTests/BookControllerUnitTest.java
+++ biblioflex-api/src/test/java/com/example/qualite/ControllerTests/BookControllerUnitTest.java
@@ -1,34 +1,37 @@
 package com.example.qualite.ControllerTests;
 
-import lombok.AllArgsConstructor;
+import com.example.qualite.controller.BookController;
+import com.example.qualite.repository.BookRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(BookController.class)
 class BookControllerUnitTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private BookRepository bookRepository;
+
     @Test
-    @DisplayName("GET /reservations should return status OK")
+    @DisplayName("GET /books should return status OK")
     void testGetAll() throws Exception {
-        mockMvc.perform(get("/reservations"))
+        mockMvc.perform(get("/books"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("GET /reservations/{id} should return client error (if not found)")
+    @DisplayName("GET /books/{id} should return client error (if not found)")
     void testGetByIdNotFound() throws Exception {
-        mockMvc.perform(get("/reservations/9999"))
+        mockMvc.perform(get("/books/9999"))
                 .andExpect(status().is4xxClientError());
     }
 }
--- biblioflex-api/src/test/java/com/example/qualite/ControllerTests/ClientControllerUnitTest.java
+++ biblioflex-api/src/test/java/com/example/qualite/ControllerTests/ClientControllerUnitTest.java
@@ -1,21 +1,25 @@
 package com.example.qualite.ControllerTests;
 
+import com.example.qualite.controller.ClientController;
+import com.example.qualite.repository.ClientRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ClientController.class)
 class ClientControllerUnitTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ClientRepository clientRepository;
 
     @Test
     @DisplayName("GET /clients should return 200 OK")
